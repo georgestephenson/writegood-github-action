1cc13d5982501d21f91d90a97949cb436af16bab
georgestephenson <gf.s@hotmail.com>
2025-05-25 11:48:35 +0100

    Fix globbing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -eux
 
+shopt -s globstar nullglob
+
 echo "::group::Files to check"
-ls -1 ./*.md || true
+printf '%s\n' **/*.md
 echo "::endgroup::"
 
 echo "::group::write-good suggestions"
-if write-good ./*.md; then
+if write-good **/*.md; then
   echo "✅ No suggestions"
 else
   echo "❌ Suggestions above"
