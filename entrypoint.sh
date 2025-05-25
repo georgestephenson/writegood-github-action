10f0e39b3e466c6ac4e6878640ff742b3194b63e
georgestephenson <gf.s@hotmail.com>
2025-05-25 11:39:49 +0100

    Improvements to write-good job

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-set -e
+set -eux
 
-write-good ./*.md
+echo "::group::Files to check"
+ls -1 ./*.md || true
+echo "::endgroup::"
+
+echo "::group::write-good suggestions"
+if write-good ./*.md; then
+  echo "✅ No suggestions"
+else
+  echo "❌ Suggestions above"
+  exit 1
+fi
+echo "::endgroup::"
